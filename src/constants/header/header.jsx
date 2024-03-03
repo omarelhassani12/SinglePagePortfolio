4bf954f9200d5b2537cae6c1a6c2b70ec53c9f85
Omar EL HASSANI <94725110+omarelhassani12@users.noreply.github.com>
2024-03-03 15:50:24 +0100

    add the responsive styles

--- src/constants/header/header.jsx
+++ src/constants/header/header.jsx
@@ -1,21 +1,60 @@
-import React from 'react';
+// import React from 'react';
+// import './header.css';
+// import oeLogo from '../../assets/oe.ico';
+// import scrollToSection from '../../utils/scrollUtils';
+
+// function Header() {
+//   return (
+//     <div id="header">
+//       <div className="logo">
+//         <a href="/"><img src={oeLogo} alt="OE Logo" /></a>
+//       </div>
+//       <ul className='navbar'>
+//         <li><a href="/" onClick={(e) => scrollToSection('about', e)}>About</a></li>
+//         {/* <li><a href="/" onClick={(e) => scrollToSection('passion', e)}>Passion</a></li> */}
+//         <li><a href="/" onClick={(e) => scrollToSection('experience', e)}>Experiences</a></li>
+//         <li><a href="/" onClick={(e) => scrollToSection('work', e)}>Works</a></li>
+//         <li><a href='/' id='hireMe' onClick={(e) => scrollToSection('footer', e)}>Hire me</a></li>
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default Header;
+
+
+import React, { useState } from 'react';
 import './header.css';
 import oeLogo from '../../assets/oe.ico';
 import scrollToSection from '../../utils/scrollUtils';
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+    console.log('Menu toggled. isOpen:', isOpen);
+  };
+
   return (
     <div id="header">
       <div className="logo">
         <a href="/"><img src={oeLogo} alt="OE Logo" /></a>
       </div>
-      <ul className='navbar'>
-        <li><a href="/" onClick={(e) => scrollToSection('about', e)}>About</a></li>
-        {/* <li><a href="/" onClick={(e) => scrollToSection('passion', e)}>Passion</a></li> */}
-        <li><a href="/" onClick={(e) => scrollToSection('experience', e)}>Experiences</a></li>
-        <li><a href="/" onClick={(e) => scrollToSection('work', e)}>Works</a></li>
-        <li><a href='/' id='hireMe' onClick={(e) => scrollToSection('footer', e)}>Hire me</a></li>
-      </ul>
+      <div className={`navbar ${isOpen ? 'open' : ''}`}>
+        <div className="hamburger" onClick={toggleMenu}>
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+        </div>
+        <ul>
+          <li><a href="/" onClick={(e) => scrollToSection('about', e)}>About</a></li>
+          {/* <li><a href="/" onClick={(e) => scrollToSection('passion', e)}>Passion</a></li> */}
+          <li><a href="/" onClick={(e) => scrollToSection('experience', e)}>Experiences</a></li>
+          <li><a href="/" onClick={(e) => scrollToSection('work', e)}>Works</a></li>
+          <li><a href='/' id='hireMe' onClick={(e) => scrollToSection('footer', e)}>Hire me</a></li>
+        </ul>
+      </div>
     </div>
   );
 }
